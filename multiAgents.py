4476e736cfb216e9ee4eb24d3c4f01b19fcb4ff8
Daniel A. Thompson <github@danielthompson.net>
2016-12-01 12:40:04 -0600

    Question 5 Partial Implementation

--- multiAgents.py
+++ multiAgents.py
@@ -18,6 +18,7 @@ import random, util
 
 from game import Agent
 
+
 class ReflexAgent(Agent):
     """
       A reflex agent chooses an action at each choice point by examining
@@ -27,7 +28,6 @@ class ReflexAgent(Agent):
       it in any way you see fit, so long as you don't touch our method
       headers.
     """
-
 
     def getAction(self, gameState):
         """
@@ -45,7 +45,7 @@ class ReflexAgent(Agent):
         scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
         bestScore = max(scores)
         bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-        chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+        chosenIndex = random.choice(bestIndices)  # Pick randomly among the best
 
         "Add more of your code here if you want to"
 
@@ -120,8 +120,6 @@ class ReflexAgent(Agent):
             if minDistanceToFood > distance:
                 minDistanceToFood = distance
 
-
-
         returnScoreElements.append(- 100 * minDistanceToFood)
 
         returnScore = 0
@@ -129,6 +127,7 @@ class ReflexAgent(Agent):
             returnScore += element
 
         return returnScore
+
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -139,6 +138,7 @@ def scoreEvaluationFunction(currentGameState):
       (not reflex agents).
     """
     return currentGameState.getScore()
+
 
 class MultiAgentSearchAgent(Agent):
     """
@@ -155,15 +155,17 @@ class MultiAgentSearchAgent(Agent):
       is another abstract class.
     """
 
-    def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
-        self.index = 0 # Pacman is always agent index 0
+    def __init__(self, evalFn='scoreEvaluationFunction', depth='2'):
+        self.index = 0  # Pacman is always agent index 0
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
+
 
 class MinimaxAgent(MultiAgentSearchAgent):
     """
       Your minimax agent (question 2)
     """
+
     def maxValue(self, gameState, agentIndex, myDepth):
         v = -1e309
         legalActions = gameState.getLegalActions(agentIndex)
@@ -327,10 +329,36 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
 
         return bestAction
 
+
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
       Your expectimax agent (question 4)
     """
+
+    def maxValueRand(self, gameState, agentIndex, myDepth):
+        v = -1e309
+        legalActions = gameState.getLegalActions(agentIndex)
+
+        bestActions = []
+
+        for action in legalActions:
+            successorState = gameState.generateSuccessor(agentIndex, action);
+            successorStateValue = self.stateValue(successorState, myDepth + 1)[1]
+
+            if successorStateValue == v:
+                bestActions.append(action)
+
+            if successorStateValue > v:
+                v = successorStateValue
+                bestActions = [action]
+
+        from random import randint
+        index = randint(0, len(bestActions) - 1)
+
+        bestAction = bestActions[index]
+
+        return [bestAction, v]
+
     def maxValue(self, gameState, agentIndex, myDepth):
         v = -1e309
         legalActions = gameState.getLegalActions(agentIndex)
@@ -382,7 +410,6 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         else:
             return self.avgValue(gameState, agentIndex, myDepth)
 
-
     def getAction(self, gameState):
         """
           Returns the expectimax action using self.depth and self.evaluationFunction
@@ -396,18 +423,13 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
 
         return bestAction
 
-
 def betterEvaluationFunction(currentGameState):
-    """
-      Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
-      evaluation function (question 5).
-
-      DESCRIPTION: <write something here so we know what you did>
-    """
     newPos = currentGameState.getPacmanPosition()
     newFood = currentGameState.getFood()
     newGhostStates = currentGameState.getGhostStates()
     newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+
+    "*** YOUR CODE HERE ***"
 
     returnScoreElements = []
 
@@ -415,19 +437,20 @@ def betterEvaluationFunction(currentGameState):
     # gameStateScore = successorGameState.getScore()
     # returnScoreElements.append(gameStateScore)
 
-    numFood = currentGameState.getNumFood()
-
+    # number of food pellets remaining - less is better
+    numFood = 0
+    for food in newFood.asList():
+        if food:
+            numFood += 1
     returnScoreElements.append(- 10000 * numFood)
 
     if numFood == 0:
         return 1000000
 
-    ghostStates = currentGameState.getGhostStates()
-
     # max dist to ghost - more is better
     maxDistanceToGhost = 0
-    for ghostState in ghostStates:
-        distance = manhattanDistance(ghostState.configuration.pos, newPos)
+    for state in newGhostStates:
+        distance = manhattanDistance(state.configuration.pos, newPos)
         if maxDistanceToGhost < distance:
             maxDistanceToGhost = distance
 
@@ -460,7 +483,208 @@ def betterEvaluationFunction(currentGameState):
         returnScore += element
 
     return returnScore
+#
+# def betterEvaluationFunctionTry(currentGameState):
+#     """
+#       Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
+#       evaluation function (question 5).
+#
+#       DESCRIPTION: <write something here so we know what you did>
+#     """
+#
+#     pacmanPosition = currentGameState.getPacmanPosition()
+#     foods = currentGameState.getFood()
+#     ghostStates = currentGameState.getGhostStates()
+#     capsules = currentGameState.getCapsules()
+#     scaredTimes = [ghostState.scaredTimer for ghostState in ghostStates]
+#     gameStateScore = currentGameState.getScore()
+#     numFood = currentGameState.getNumFood()
+#     ghostStates = currentGameState.getGhostStates()
+#
+#     returnScoreElements = []
+#
+#     # max dist to ghost - more is better
+#     maxDistanceToGhost = 0
+#     for ghostState in ghostStates:
+#         if ghostState.scaredTimer > 0:
+#             maxDistanceToGhost = 1e309
+#         else:
+#             distance = manhattanDistance(ghostState.getPosition(), pacmanPosition)
+#             if maxDistanceToGhost < distance:
+#                 maxDistanceToGhost = distance
+#
+#     # max dist to food - less is better
+#     maxDistanceToFood = 0
+#     for food in foods.asList():
+#         distance = manhattanDistance(food, pacmanPosition) + 2
+#         if maxDistanceToFood < distance:
+#             maxDistanceToFood = distance
+#
+#     # sum of all distances to food
+#     sumDistanceToFood = 0
+#     for food in foods.asList():
+#         sumDistanceToFood += manhattanDistance(food, pacmanPosition) + 2
+#
+#     # min dist to food - less is better
+#     minDistanceToFood = 1e309
+#     for food in foods.asList():
+#         distance = manhattanDistance(food, pacmanPosition) + 2
+#         if minDistanceToFood > distance:
+#             minDistanceToFood = distance
+#
+#     minDistanceToCapsule = 1e309
+#     for capsule in capsules:
+#         distance = manhattanDistance(capsule, pacmanPosition) + 2
+#         if minDistanceToCapsule > distance:
+#             minDistanceToCapsule = distance
+#
+#     closestFoodMazeDistance = mazeDistance(currentGameState)
+#
+#     returnScoreElements.append(maxDistanceToGhost)
+#     returnScoreElements.append(-100 * numFood)
+#     # returnScoreElements.append(-20 * closestFoodMazeDistance)
+#     # returnScoreElements.append(-10 * minDistanceToFood)
+#     # returnScoreElements.append(-1 * maxDistanceToFood)
+#     # returnScoreElements.append(-1 * minDistanceToCapsule)
+#     returnScoreElements.append(-10 * len(capsules))
+#     returnScoreElements.append(-5 * gameStateScore)
+#
+#     if numFood == 0:
+#         return 1e309
+#
+#     # if pacman is right next to a ghost, flee
+#     if maxDistanceToGhost <= 2:
+#         return -1e309
+#
+#     returnScore = 0
+#     for element in returnScoreElements:
+#         returnScore += element
+#
+#     return returnScore
+#
+#
+# # Abbreviation
+# better = betterEvaluationFunction
+#
+# from game import Actions
+#
+#
+# class FoodSearchProblem:
+#     """
+#     A search problem associated with finding the a path that collects all of the
+#     food (dots) in a Pacman game.
+#
+#     A search state in this problem is a tuple ( pacmanPosition, foodGrid ) where
+#       pacmanPosition: a tuple (x,y) of integers specifying Pacman's position
+#       foodGrid:       a Grid (see game.py) of either True or False, specifying remaining food
+#     """
+#
+#     def __init__(self, startingGameState):
+#         self.start = (startingGameState.getPacmanPosition(), startingGameState.getFood())
+#         self.walls = startingGameState.getWalls()
+#         self.startingGameState = startingGameState
+#         self._expanded = 0  # DO NOT CHANGE
+#         self.heuristicInfo = {}  # A dictionary for the heuristic to store information
+#
+#     def getStartState(self):
+#         return self.start
+#
+#     def isGoalState(self, state):
+#
+#         oldFoodList = self.start[1].asList()
+#         numOldFood = len(oldFoodList)
+#
+#         newFoodList = state[1].asList()
+#         numNewFood = len(newFoodList)
+#
+#         return numNewFood < numOldFood
+#
+#     def getSuccessors(self, state):
+#         "Returns successor states, the actions they require, and a cost of 1."
+#         successors = []
+#         self._expanded += 1  # DO NOT CHANGE
+#         for direction in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
+#             x, y = state[0]
+#             dx, dy = Actions.directionToVector(direction)
+#             nextx, nexty = int(x + dx), int(y + dy)
+#             if not self.walls[nextx][nexty]:
+#                 nextFood = state[1].copy()
+#                 nextFood[nextx][nexty] = False
+#                 successors.append((((nextx, nexty), nextFood), direction, 1))
+#         return successors
+#
+#     def getCostOfActions(self, actions):
+#         """Returns the cost of a particular sequence of actions.  If those actions
+#         include an illegal move, return 999999"""
+#         x, y = self.getStartState()[0]
+#         cost = 0
+#         for action in actions:
+#             # figure out the next state and see whether it's legal
+#             dx, dy = Actions.directionToVector(action)
+#             x, y = int(x + dx), int(y + dy)
+#             if self.walls[x][y]:
+#                 return 999999
+#             cost += 1
+#         return cost
+#
+#
+# def breadthFirstSearch(problem):
+#     """Search the shallowest nodes in the search tree first."""
+#     "*** YOUR CODE HERE ***"
+#
+#     # closed <- an empty set
+#     closed = set()
+#
+#     # fringe <- insert(make-node(initial-state[problem]), fringe)
+#     fringe = util.Queue()
+#     fringe.push([problem.getStartState(), []])
+#
+#     while True:
+#         # if fringe is empty then return failure
+#         if fringe.isEmpty():
+#             return None
+#
+#         # node <- remove-front(fringe, strategy)
+#         node, actions = fringe.pop()
+#
+#         # if goal-test(problem, state[node])
+#         if problem.isGoalState(node):
+#             return actions
+#
+#         # if state[node] is not in closed
+#         if node not in closed:
+#             # add state[node] to closed
+#             closed.add(node)
+#
+#             # for child-node in expand(state[node], problem)
+#             for successor, action, stepCost in problem.getSuccessors(node):
+#                 # fringe <- insert(child-node, fringe)
+#
+#                 fringe.push((successor, actions + [action]))
+#
+#
+# def mazeDistance(gameState):
+#     """
+#     Returns the maze distance between pacman and the closest food pellet
+#     you have already built. The gameState can be any game state -- Pacman's
+#     position in that state is ignored.
+#
+#     Example usage: mazeDistance( (2,4), (5,6), gameState)
+#
+#     This might be a useful helper function for your ApproximateSearchAgent.
+#     """
+#     # x1, y1 = point1
+#     # x2, y2 = point2
+#     # walls = gameState.getWalls()
+#     # assert not walls[x1][y1], 'point1 is a wall: ' + str(point1)
+#     # assert not walls[x2][y2], 'point2 is a wall: ' + str(point2)
+#     # prob = PositionSearchProblem(gameState, start=point1, goal=point2, warn=False, visualize=False)
+#
+#     prob = FoodSearchProblem(gameState)
+#     path = breadthFirstSearch(prob)
+#     if path is None:
+#         return 0
+#
+#     return len(path)
 
-# Abbreviation
-better = betterEvaluationFunction
 
